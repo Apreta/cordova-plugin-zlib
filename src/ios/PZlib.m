74b090ca3e62e16df68e1e81adf2aeb2975265ed
Mike <quicklyfrozen@gmail.com>
2016-06-12 20:46:23 -0400

    remove test method

--- src/ios/PZlib.m
+++ src/ios/PZlib.m
@@ -56,19 +56,6 @@
   }
 }
 
-- (void)test:(CDVInvokedUrlCommand*)command
-{
-
-    NSString* name = [[command arguments] objectAtIndex:0];
-    NSString* msg = [NSString stringWithFormat: @"Hello, %@", name];
-
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
-
-    [self.commandDelegate sendPluginResult:result callbackId:command.callbackId];
-}
-
 - (void)deflate:(CDVInvokedUrlCommand*)command
 {
     CDVPluginResult* result;
