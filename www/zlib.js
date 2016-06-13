c694ef45f728d26e203e1b9693105355c4ab4638
Mike <quicklyfrozen@gmail.com>
2016-06-12 21:11:22 -0400

    remove another unneeded method

--- www/zlib.js
+++ www/zlib.js
@@ -1,9 +1,6 @@
 /*global cordova, module*/
 
 module.exports = {
-    test: function (name, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "Zlib", "test", [name]);
-    },
     compress: function (stream, data, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "Zlib", "deflate", [stream, data]);
     },
